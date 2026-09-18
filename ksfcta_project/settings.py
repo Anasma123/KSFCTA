4805b0e4ccedb9e60bf30aeb5868a79d21ecfbfa
anasjhfit <anas@jhfitinnovations.com>
2026-09-18 12:27:22 +0530

    four

--- ksfcta_project/settings.py
+++ ksfcta_project/settings.py
@@ -13,6 +13,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+    'http://127.0.0.1',
+    'http://localhost',
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
